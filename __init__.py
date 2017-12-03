3695a180a1271e8bd25ce258f74ebcf854cf7a7d
Michael Szegedy <mszegedy2@gmail.com>
2017-12-03 03:24:27 -0500

    Added constraints file reader, and more convenient pose import functions.

--- __init__.py
+++ __init__.py
@@ -9,6 +9,19 @@ import os
 import fcntl, itertools, json, re, time
 import pyrosetta as pr
 
+def add_constraints_from_file(pose, filename):
+    '''Reads a new-style (non-enzdes) .cst file and adds the constraints in it
+    to a pose.
+    '''
+    # For the curious: passing pose doesn't actually accomplish anything. It's
+    # required because they were gonna have this function overwrite the
+    # constraint set of the pose at the end, but as of the time of this
+    # writing, that line is commented out.
+    cst = pr.rosetta.core.scoring.constraints.ConstraintIO \
+            .read_constraints_new(filename, pose.constraint_set(), pose)
+    pose.constraint_set(cst)
+    return cst
+
 def atom_dist(pose, atom1, atom2):
     '''Takes in a pose and two AtomIDs. Returns the distance of the two atoms from
     each other.
@@ -78,37 +91,57 @@ def dump_numbered_pdb(pose, prefix, directory=None):
             os.remove(lockfile_path)
             raise
 
-def pose_from_file_with_params(filename, params_filenames):
+def pose_from_file_with_params(filename, params):
     '''This will create a Pose() object from a PDB file that needs params. The
     params are to be given as a list of string literals of filenames.
     '''
     pose = pr.Pose()
-    pr.generate_nonstandard_residue_set(pose, params_filenames)
+    pr.generate_nonstandard_residue_set(pose, params)
     pr.pose_from_file(pose, filename)
     return pose
 
-def pose_from_pdbstring_with_params(pdbstring, params_filenames):
+def pose_from_pdbstring_with_params(pdbstring, params):
     '''This will create a Pose() object from a PDB contents string that needs
     params. The params are to be given as a list of string literals of
     filenames.
     '''
     pose = pr.Pose()
-    pr.generate_nonstandard_residue_set(pose, params_filenames)
+    pr.generate_nonstandard_residue_set(pose, params)
     pr.rosetta.core.import_pose.pose_from_pdbstring(pose, pdbstring)
     return pose
+
+def pose_from_file(filename, params=None):
+    '''This will create a Pose() object from a PDB file that needs params. The
+    params are to be given as a list of string literals of filenames.
+    '''
+    if params is not None:
+        return pose_from_file_with_params(filename, params)
+    else:
+        return pr.pose_from_file(pr.Pose(), filename)
+
+def pose_from_pdbstring(pdbstring, params=None):
+    '''This will create a Pose() object from a PDB contents string that needs
+    params. The params are to be given as a list of string literals of
+    filenames.
+    '''
+    if params is not None:
+        return pose_from_pdbstring_with_params(pdbstring, params)
+    else:
+        return pr.rosetta.core.import_pose.pose_from_pdbstring(pr.Pose(),
+                                                               pdbstring)
 
 def res_neighbors_p(pose, resnum1, resnum2, coarsep=False, bound=None):
     '''This compares the conformations of two residues in a pose, and returns a
     boolean of whether or not they are physically close to each other. There
     are two possible standards that the algorithm employs:
         coarsep=False: any two non-hydrogen atoms in the two residues'
-          sections of protein are 4.5 Ao or less apart
+          sections of protein are 4.2 Ao or less apart
         coarsep=True: CA atoms of residues are 8 Ao or less apart
     '''
     # get ready for some lisp bullshit
     bound = bound or \
             (coarsep and 8.) or \
-            4.5
+            4.2
     conformation = pose.conformation()
     if coarsep:
         # dunno what kind of residue doesn't have CA as the second atom, but we
